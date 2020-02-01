72c300fa771b1a6cee37a7b2ed1e9e8b946606ad
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-01 22:58:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
+//define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten (name) {
+  name = "Ralph";
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  name = "Bob"
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+name = ["Broom"];
+  return kittens.concat(name);
+}
+
+function prependKitten(name) {
+  return 
+}
